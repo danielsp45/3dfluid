7a472ac6147f879fa2efad1e0e7fd564d6e3407e
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2024-10-11 01:10:03 +0100

    Reformat code to 4 spaces

--- fluid_solver.h
+++ fluid_solver.h
@@ -3,6 +3,7 @@
 
 void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v,
                float *w, float diff, float dt);
+
 void vel_step(int M, int N, int O, float *u, float *v, float *w, float *u0,
               float *v0, float *w0, float visc, float dt);
 
