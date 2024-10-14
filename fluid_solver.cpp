7d728a2b31733abdaf0fdabd7485f905fc2a1134
Rui Lopes <ruilopesm19@gmail.com>
2024-10-14 18:42:25 +0100

    perf(diffuse): alter order of looping

--- fluid_solver.cpp
+++ fluid_solver.cpp
@@ -130,9 +130,9 @@ void diffuse(int M, int N, int O, int b, float *x, float *x0, float diff, float 
 void advect(int M, int N, int O, int b, float *d, float *d0, float *u, float *v, float *w, float dt) {
     float dtX = dt * M, dtY = dt * N, dtZ = dt * O;
 
-    for (int i = 1; i <= M; i++) {
+    for (int k = 1; k <= O; k++) {
         for (int j = 1; j <= N; j++) {
-            for (int k = 1; k <= O; k++) {
+            for (int i = 1; i <= M; i++) {
                 float x = i - dtX * u[IX(i, j, k)];
                 float y = j - dtY * v[IX(i, j, k)];
                 float z = k - dtZ * w[IX(i, j, k)];
